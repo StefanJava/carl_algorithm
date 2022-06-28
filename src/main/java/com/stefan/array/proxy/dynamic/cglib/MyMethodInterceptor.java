42761ed0594fa13c3c61d21540861f8d43c5fc34
stefanyang <439354223@qq.com>
2022-06-28 21:19:33 +0800

    20220627-cglib-dependency update

--- src/main/java/com/stefan/array/proxy/dynamic/cglib/MyMethodInterceptor.java
+++ src/main/java/com/stefan/array/proxy/dynamic/cglib/MyMethodInterceptor.java
@@ -3,7 +3,6 @@ package com.stefan.array.proxy.dynamic.cglib;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
-import java.lang.invoke.MethodHandle;
 import java.lang.reflect.Method;
 
 public class MyMethodInterceptor implements MethodInterceptor {
